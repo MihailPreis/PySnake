3abfe7225096ac6afbda5bd9b22257a8015d566f
MihailPreis <mihail.preis@waveaccess.ru>
2018-04-15 04:04:24 +0500

    add support game difficulty, upd setting frame, rebase code, add comments, upd readme

--- PySnake.py
+++ PySnake.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
 import random
-from time import sleep
 from collections import deque
-from PyQt5.QtWidgets import *
+from datetime import datetime
 
 import pygame
+from PyQt5.QtWidgets import *
 from pygame import *
+
+__version__ = '1.0'
 
 
 class Move:
+    """movement codes"""
     UP = 1
     RIGHT = 2
     DOWN = 3
@@ -17,17 +20,26 @@ class Move:
 
 
 class GameOverType:
+    """game over codes"""
     NULL = 0
     OUT_OF_PLACE = 1
     TOGGLE_SELF = 2
 
 
 class Game:
-    def __init__(self, width=800, height=600, wall_mode=True):
+    def __init__(self, width=800, height=600, wall_mode=True, difficulty=1):
         self.wall_mode = wall_mode
         self.pause = False
         self.__restart = False
         self.game_over = GameOverType.NULL
+        self._clock = datetime.now()
+
+        # game difficulty
+        self._tick = {
+            1: .1,
+            2: .05,
+            3: .03
+        }.get(difficulty)
 
         self.WIN_WIDTH = width
         self.WIN_HEIGHT = height
@@ -60,6 +72,10 @@ class Game:
         self.main()
 
     def get_apple(self):
+        """Generate `apple` on map
+
+        :return:
+        """
         def _():
             return random.randint(0, self.level_height - 1), \
                    random.randint(0, self.level_width - 1)
@@ -73,6 +89,17 @@ class Game:
         self.apple = True
 
     def move(self):
+        """Implements the movement of the snake relative to the current
+        direction and time of the tick (difficulty).
+
+        :return:
+        """
+        # if not enough time has passed...
+        if (datetime.now() - self._clock).total_seconds() < self._tick:
+            return
+
+        self._clock = datetime.now()
+
         y, x = self.snake_path[-1]
         if self.move_direction == Move.UP:
             y -= 1
@@ -93,6 +120,7 @@ class Game:
             self.level[old_y].pop(old_x)
 
             if self.wall_mode:
+                # we implement an infinite-looped playing field
                 if y not in range(self.level_height):
                     y = 0 if y >= self.level_height else self.level_height - 1
                 elif x not in range(self.level_width):
@@ -100,11 +128,13 @@ class Game:
 
             point = self.level[y][x]
             if point == "x":
+                # if you ate an Apple, increase the length of the snake
                 self.snake_len += 1
                 self.snake_path.appendleft((old_y, old_x))
                 self.level[old_y].insert(old_x, ".")
                 self.apple = False
             elif point == ".":
+                # if you came across your body
                 self.game_over = GameOverType.TOGGLE_SELF
             else:
                 self.level[old_y].insert(old_x, " ")
@@ -114,6 +144,11 @@ class Game:
             self.snake_path.append((y, x))
 
     def render(self, screen):
+        """Draw playing field with objects.
+
+        :param screen: current screen
+        :return:
+        """
         x = y = 0
         for row in self.level:
             for col in row:
@@ -128,6 +163,12 @@ class Game:
             x = 0
 
     def hud(self, screen, game_font):
+        """Draw hud canvas.
+
+        :param screen: current screen
+        :param game_font: font file io
+        :return:
+        """
         if self.game_over != GameOverType.NULL:
             self.pause = True
             game_over_hud = game_font.render(
@@ -158,10 +199,12 @@ class Game:
             screen.blit(points_hud, (5, 5))
 
     def main(self):
+        """main game loop"""
         pygame.init()
         pygame.font.init()
         screen = pygame.display.set_mode(self.DISPLAY)
         pygame.display.set_caption("PSnake")
+        pygame.display.set_icon(pygame.image.load("game.ico").convert_alpha())
         bg = Surface((self.WIN_WIDTH, self.WIN_HEIGHT))
         bg.fill(Color(self.BACKGROUND_COLOR))
         game_font = pygame.font.Font('game_font.ttf', 18)
@@ -207,29 +250,38 @@ class Game:
             self.hud(screen, game_font)
 
             pygame.display.update()
-            sleep(.05)
 
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
         self.__ok = False
-        self.lbl = QLabel("Resolution", self)
-        self.combo = QComboBox(self)
-        self.combo.addItems(["800x600", "1024x768"])
-        self.lbl2 = QLabel("Wall mode", self)
-        self.check = QCheckBox(self)
+        self.res_label = QLabel("Resolution", self)
+        self.res_combo = QComboBox(self)
+        self.res_combo.addItems(["800x600", "1024x768"])
+        self.dif_label = QLabel("Difficulty", self)
+        self.dif_combo = QComboBox(self)
+        self.dif_combo.addItems(["Easy", "Normal", "Hard"])
+        self.wm_label = QLabel("Wall mode", self)
+        self.wm_check = QCheckBox(self)
         self.button = QPushButton("Play", self)
         self.init_ui()
 
     def init_ui(self):
-        self.combo.move(65, 10)
-        self.lbl.move(10, 13)
-        self.check.move(65, 40)
-        self.lbl2.move(10, 40)
-        self.button.move(160, 20)
+        self.res_combo.move(65, 10)
+        self.res_label.move(10, 13)
+        self.dif_combo.move(65, 40)
+        self.dif_combo.currentIndexChanged.connect(
+            self._difficulty_handler
+        )
+        self.dif_label.move(10, 43)
+        self.wm_check.move(65, 70)
+        self.wm_label.move(10, 70)
+        self.button.resize(60, 60)
+        self.button.move(170, 20)
         self.button.clicked.connect(self.ok_close)
-        self.setGeometry(300, 300, 250, 70)
+        self.button.setFocus()
+        self.setGeometry(300, 300, 250, 100)
         self.setWindowTitle('PSnake - Conf')
         self.show()
 
@@ -237,14 +289,30 @@ class Widget(QWidget):
         self.__ok = True
         self.close()
 
+    def _difficulty_handler(self, difficulty):
+        if difficulty == 2:
+            self.wm_check.setChecked(False)
+            self.wm_check.setDisabled(True)
+        else:
+            self.wm_check.setDisabled(False)
+
     def get_params(self):
-        res = self.combo.currentText()
-        w_mode = self.check.isChecked()
-        return {"ok": self.__ok, "res": res.split("x"), "wall_mode": w_mode}
+        res = self.res_combo.currentText()
+        w_mode = self.wm_check.isChecked()
+        return {
+            "ok": self.__ok, "res": res.split("x"), "wall_mode": w_mode,
+            "difficulty": self.dif_combo.currentIndex()
+        }
 
 
 if __name__ == "__main__":
     import sys
+    """       _
+          .__(.)< (MEOW)
+    ~~~~~~~\___)~~~~~~~~
+     ~  ~ ~   ~ ~    ~ 
+    """
+
     app = QApplication(sys.argv)
     ex = Widget()
     app.exec_()
@@ -254,5 +322,6 @@ if __name__ == "__main__":
             Game(
                 width=int(args['res'][0]),
                 height=int(args['res'][1]),
-                wall_mode=args['wall_mode']
+                wall_mode=args['wall_mode'],
+                difficulty=int(args['difficulty']) + 1
             )
--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # Python Snake Game
 A little game on Python.
 
+System requirements
+-------------------
+ - Python 3.6.x
+
 How to play
----
-1. Run ``p_snake.py``
+-----------
+1. Install requirements `pip install -r requirements.txt`
+1. Run ``python PySnake.py``
 2. ?????
 3. PROFIT!1
+
+Keys
+----
+ - <kbd>&#8593;</kbd> - turn top
+ - <kbd>&#8595;</kbd> - turn down
+ - <kbd>&#8592;</kbd> - turn left
+ - <kbd>&#8594;</kbd> - turn right
+ - <kbd>ESC</kbd> - exit game
+ - <kbd>PAUSE</kbd> - pause/start game
+ - <kbd>ENTER</kbd> - in case of game over, start game again
+
+TODO
+----
+ - fix wall-mod (move left)
+ - fix snake body shift (quick turns)
+ - exclude Qt5
