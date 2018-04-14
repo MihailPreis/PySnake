d1040765b5e6e6f5c3e61ee878343c475c2ade55
MihailPreis <mihail.preis@waveaccess.ru>
2018-04-15 04:08:50 +0500

    add LICENSE.md

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 import PySnake
 
-requires_files = ['game_font.ttf', 'game.ico', 'README.md']
+requires_files = ['game_font.ttf', 'game.ico', 'README.md', 'LICENSE.md']
 requires = []
 with open('requirements.txt', 'r') as file:
     requires.extend([row for row in file.read().split('\n') if row != ''])
