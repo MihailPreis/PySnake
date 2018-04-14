70c132420ccc7ef2246ed7a861c752a4c3e63d1d
MihailPreis <mihail.preis@waveaccess.ru>
2018-04-15 04:04:37 +0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from cx_Freeze import setup, Executable
+from os.path import join, dirname
+
+import PySnake
+
+requires_files = ['game_font.ttf', 'game.ico', 'README.md']
+requires = []
+with open('requirements.txt', 'r') as file:
+    requires.extend([row for row in file.read().split('\n') if row != ''])
+
+setup(
+    name="PySnake",
+    version=PySnake.__version__,
+    description="A little game on Python.",
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    author='M.Price',
+    platforms=['Windows', 'Linux'],
+    install_requires=requires,
+    package_data={'': requires_files},
+    data_files=[('', requires_files)],
+    include_package_data=True,
+    executables=[Executable("PySnake.py", icon='game.ico')],
+    zip_safe=True
+)
